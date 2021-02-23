e4d7b77b48139c5796fa71dc8c1fc34e365a29a5
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-24 04:35:28 +0500

    Create App using Class component
    1:- updated
    	a:- src/App.js to make a simple Hello world App using class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+// Imports
+// React Imports
+import React, { Component } from "react";
+// Styles Imports
 import "./App.css";
 
-function App() {
-  return <div>Hello world</div>;
+// App component using Class
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
 
+// Component Exports
 export default App;
