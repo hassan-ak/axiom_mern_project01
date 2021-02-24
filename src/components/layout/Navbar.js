4bc486e32a18209bb9ae64c61458acb69b7eef32
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-25 03:48:09 +0500

    Convert UserItem and Navbar component to functional component
    1:- Updated
    	a:- src/components/users/UserItem.js to convert the component to functional format
    	b:- src/components/layout/Navbar.js to convert the component to functional format
    	c:- src/App.css to improve styling for the Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,31 @@
 // imports
 // React Imports
-import React, { Component } from "react";
+import React from "react";
 // PropTypes Imports
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  // Default properties
-  static defaultProps = {
-    title: "GitHub Finder",
-    icon: "fab fa-github",
-  };
-  // Type check for properties
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i class='fab fa-github'></i>
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+// NavBar component
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon}></i>
+        <span>{props.title}</span>
+      </h1>
+    </nav>
+  );
+};
 
+// Default properties
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+  icon: "fab fa-github",
+};
+// Type check for properties
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+// Component Exports
 export default Navbar;
