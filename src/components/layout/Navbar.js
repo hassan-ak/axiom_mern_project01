83e57fa926a8209d3961486447f3835f587ca27e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-25 02:16:45 +0500

    Properties in React and Font Awsome
    1:- Updated
    	a:- src/app.js to add properties to Navbar Component
    	b:- src/components/layout/Navbar.js
    		i:- use properties from component call
    		ii:- defined static properties
    		iii:- use type defination for properties types
    		iv:- use icon from Font Awsome
    	c:- public/index.html to add font awsome script so icons could be used in the app
    	d:- src/App.css to define styling of the app

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,28 @@
+// imports
+// React Imports
 import React, { Component } from "react";
+// PropTypes Imports
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
+  // Default properties
+  static defaultProps = {
+    title: "GitHub Finder",
+    icon: "fab fa-github",
+  };
+  // Type check for properties
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i class='fab fa-github'></i>
+          {this.props.title}
+        </h1>
+      </nav>
     );
   }
 }
