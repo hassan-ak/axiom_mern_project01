4b4ec3bf3851bb3852bf93e5f1585dba151138fd
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-25 00:58:58 +0500

    Basic React concepts
    1:- Updated
    	a:- src/App.js to
    		i:- use a variable in the code
    		ii:- use function (render based) in code
    		iii:- use function (class based) in the code
    		iv:- component based on some conditions
    		v:- use of terniary operator while using conditions
    		vi:- Multiple conditions
    		vii:- using logical operator while checking conditions

--- src/App.js
+++ src/App.js
@@ -6,10 +6,40 @@ import "./App.css";
 
 // App component using Class
 class App extends Component {
+  // Class Function
+  ageFunction = () => "28";
   render() {
+    // using a variable in the App
+    const name = "Hassan";
+    // function declaration in reatct
+    const nameFunction = () => "Zubair";
+    // While loading data from APi how to use loading functionality
+    const loading = false;
+    const showName = true;
+    if (loading) {
+      return <h3>Loading...</h3>;
+    }
     return (
       <div>
-        <h1>Hello world</h1>
+        {/* using a varibale or JS expression in the code */}
+        <h1>Hello {name.toUpperCase()}!</h1>
+        {/* using some function in code */}
+        <h2>Hello {nameFunction()}!</h2>
+        {/* using class function in code */}
+        <h3>
+          {nameFunction()} is {this.ageFunction} years old.
+        </h3>
+        {/* using ternary operator for conditions */}
+        {loading ? <h4>Loading...</h4> : <h4>Loaded</h4>}
+        {/* Checking for multiple coditions */}
+        {/* case one */}
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+          <h4>Hello {showName ? name : null}</h4>
+        )}
+        {/* case two */}
+        {loading ? <h4>Loading...</h4> : <h4>Hello {!showName && name}</h4>}
       </div>
     );
   }
