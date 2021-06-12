d7f44efa23f582fc424e1f4bb0c2ca3c1a94c017
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-06-12 12:35:56 +0500

    Step 05:- Create Navbar Component
    1:- Created
    	a:- src/components/layout/Navbar.js to define Navbar component
    2:- Updated
    	a:- src/app.js to add Navbar component in the app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 // Imports
 // React Imports
 import React, { Component } from "react";
+// Component Imports
+import Navbar from "./components/layout/Navbar";
 // Styles Imports
 import "./App.css";
 
 // App component using Class
 class App extends Component {
-  // Class Function
-  ageFunction = () => "28";
   render() {
-    // using a variable in the App
-    const name = "Hassan";
-    // function declaration in reatct
-    const nameFunction = () => "Zubair";
-    // While loading data from APi how to use loading functionality
-    const loading = false;
-    const showName = true;
-    if (loading) {
-      return <h3>Loading...</h3>;
-    }
     return (
       <div>
-        {/* using a varibale or JS expression in the code */}
-        <h1>Hello {name.toUpperCase()}!</h1>
-        {/* using some function in code */}
-        <h2>Hello {nameFunction()}!</h2>
-        {/* using class function in code */}
-        <h3>
-          {nameFunction()} is {this.ageFunction()} years old.
-        </h3>
-        {/* using ternary operator for conditions */}
-        {loading ? <h4>Loading...</h4> : <h4>Loaded</h4>}
-        {/* Checking for multiple coditions */}
-        {/* case one */}
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h4>Hello {showName ? name : null}</h4>
-        )}
-        {/* case two */}
-        {loading ? <h4>Loading...</h4> : <h4>Hello {!showName && name}</h4>}
+        <Navbar />
       </div>
     );
   }
