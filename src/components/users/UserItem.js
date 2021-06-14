bfcbf500bddad6daa35024d3a2556ad298aa5468
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-06-14 16:10:09 +0500

    Step 08:- States in React (constructor function)
    1:- Updated
    	a:- src/components/users/UserItem.js to define a state having multiple hard-coded properties of a user which then be used in the component to display user data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,34 @@
+// Imports
+// React Imports
 import React, { Component } from "react";
 
-export class UserItem extends Component {
+// UserItem component
+class UserItem extends Component {
+  // constructor function
+  constructor() {
+    super();
+    this.state = {
+      login: "mojombo",
+      id: 1,
+      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo",
+    };
+  }
   render() {
-    return <div>UserItem</div>;
+    return (
+      <div className='card text-center'>
+        <img
+          src={this.state.avatar_url}
+          alt='Profile'
+          className='round-img'
+          style={{ width: "60px" }}
+        />
+        <h3>{this.state.login}</h3>
+        <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+          Profile
+        </a>
+      </div>
+    );
   }
 }
 
