b4c6262301ec5afbd0583ae28c3966661e311d61
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-06-15 20:33:05 +0500

    Step 09:- States in React (latest approch)
    1:- Updated
    	a:- src/components/users/UserItem.js
    		i:- define a state having multiple hard-coded properties of a user which then be used in the component to display user data
    		ii:- destructing to use state data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -4,27 +4,26 @@ import React, { Component } from "react";
 
 // UserItem component
 class UserItem extends Component {
-  // constructor function
-  constructor() {
-    super();
-    this.state = {
-      login: "mojombo",
-      id: 1,
-      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo",
-    };
-  }
+  // States
+  state = {
+    login: "mojombo",
+    id: 1,
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+  };
   render() {
+    // Destructring to use state data
+    const { login, avatar_url, html_url } = this.state;
     return (
       <div className='card text-center'>
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt='Profile'
           className='round-img'
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
-        <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+        <h3>{login}</h3>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
           Profile
         </a>
       </div>
