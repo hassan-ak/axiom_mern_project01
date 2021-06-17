c56ebc4d0787edda36d26a8194f4714fa1fccd0e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-06-17 17:12:43 +0500

    Step 11:- Convert UserItem and Navbar component to functional component
    1:- Updated
    	a:- src/components/users/UserItem.js to convert the component to functional format
    	b:- src/components/layout/Navbar.js to convert the component to functional format
    	c:- src/App.css to improve styling for the Navbar

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,31 @@
 // Imports
 // React Imports
-import React, { Component } from "react";
+import React from "react";
+// Prop types Imports
+import PropTypes from "prop-types";
 
 // UserItem component
-class UserItem extends Component {
-  render() {
-    // Destructring to use state data
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt='Profile'
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
-          Profile
-        </a>
-      </div>
-    );
-  }
-}
+// Destructring for props directly
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt='Profile'
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        Profile
+      </a>
+    </div>
+  );
+};
+
+// Type Checking
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
