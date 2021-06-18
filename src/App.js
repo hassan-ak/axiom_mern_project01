dc4f7ba74140fd65d5fbfe1b28e6c7082246064c
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-06-18 23:07:17 +0500

    Step 12:-  Fetch data from api
    1:- Installed
    	a:- Axios
    		i:- uisng "yarn add axios" to fetch data from the Api
    2:- Updated
    	a:- src/app.js
    		i: - to fecth data from Api using axios and console logged it to view the result

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import React, { Component } from "react";
 // Component Imports
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+// Axios imports
+import axios from "axios";
 // Styles Imports
 import "./App.css";
 
 // App component using Class
 class App extends Component {
+  // This function execute when ever the component renders
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users")
+      .then((res) => console.log(res.data));
+  }
   render() {
     return (
       <div>
